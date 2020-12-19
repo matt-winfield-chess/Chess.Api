c411c12b4df77b92d4420f7ccff2867ff0808591
Matthew Winfield <matthew.thomas.winfield@gmail.com>
2020-12-19 15:08:18 +0000

    Implement draw offer functionality

--- Chess.Api/Constants/GameConstants.cs
+++ Chess.Api/Constants/GameConstants.cs
@@ -3,10 +3,15 @@
     public static class GameConstants
     {
         public const string STANDARD_START_POSITION_FEN = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
+
+        public const string WHITE = "white";
+        public const string BLACK = "black";
+
         public const string CHECKMATE_TERMINATION = "checkmate";
         public const string STALEMATE_TERMINATION = "stalemate";
         public const string FIFTY_MOVE_RULE_TERMINATION = "50-move-rule";
         public const string THREEFOLD_REPETITION_TERMINATION = "threefold-repetition";
         public const string RESIGNATION_TERMINATION = "resignation";
+        public const string AGREEMENT_TERMINATION = "agreement";
     }
 }
--- Chess.Api/SignalR/Messages/GameHubOutgoingMessages.cs
+++ Chess.Api/SignalR/Messages/GameHubOutgoingMessages.cs
@@ -8,5 +8,8 @@
         public const string DRAW = "Draw";
         public const string MOVE_RECEIVED = "MoveReceived";
         public const string RESIGNATION = "Resignation";
+        public const string DRAW_OFFER = "DrawOffer";
+        public const string DRAW_OFFER_ACCEPTED = "DrawOfferAccepted";
+        public const string DRAW_OFFER_DECLINED = "DrawOfferDeclined";
     }
 }
